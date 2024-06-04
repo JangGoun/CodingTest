7cfbcb0844b4a5d601f35ef40ab0ddf09926911a
JangGoun <goun3596@gmail.com>
2024-06-04 16:35:11 +0900

    day02

---
+++ src/Day02/d203.java
@@ -0,0 +1,15 @@
+package Day02;
+
+import java.util.Scanner;
+
+public class d203 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+
+        for (int i = 0; i< a.length(); i++){
+            System.out.println(a.charAt(i));
+        }
+
+    }
+}
