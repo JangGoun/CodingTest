01dbd6ae80370757637e44d47ff8ee40d35be3cd
JangGoun <goun3596@gmail.com>
2024-06-05 16:44:52 +0900

    day03

---
+++ src/Day03/d302.java
@@ -0,0 +1,13 @@
+package Day03;
+
+public class d302 {
+    public String solution(String[] arr) {
+        String answer = "";
+
+        for (int i = 0; i < arr.length; i++) {
+            answer += arr[i];
+        }
+
+        return answer;
+    }
+}
---
+++ src/Day03/d303.java
@@ -0,0 +1,12 @@
+package Day03;
+
+public class d303 {
+    public String solution(String my_string, int k) {
+        String answer = "";
+
+        for (int i = 0; i < k; i++) {
+            answer += my_string;
+        }
+        return answer;
+    }
+}
