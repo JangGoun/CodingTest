c0da094f35fcf14b5756b7c68bff5a67322d12b2
JangGoun <goun3596@gmail.com>
2024-06-10 10:28:55 +0900

    day06

---
+++ src/Day06/d604.java
@@ -0,0 +1,22 @@
+package Day06;
+
+public class d604 {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = {};
+        int temp = 0;
+
+
+        for (int i = 0; i < queries.length; i++) {
+
+            temp = arr[queries[i][0]];
+            arr[queries[i][0]] = arr[queries[i][1]];
+            arr[queries[i][1]]=temp;
+
+        }
+
+
+        answer = arr;
+
+        return answer;
+    }
+}
