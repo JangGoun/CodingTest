a23eba3a2ca64c72bfcd8fb33b62432e4b7966b2
JangGoun <goun3596@gmail.com>
2024-06-14 16:13:19 +0900

    day08

---
+++ src/Day08/d803.java
@@ -0,0 +1,18 @@
+package Day08;
+
+public class d803 {
+    public String solution(String my_string, int[] index_list) {
+        String answer = "";
+        for (int i = 0; i < index_list.length; i++) {
+            answer += my_string.charAt(index_list[i]);
+        }
+        return answer;
+
+//        StringBuilder sb = new StringBuilder();
+//
+//        for (int idx : index_list)
+//            sb.append(my_string.charAt(idx));
+//
+//        return sb.toString();
+    }
+}
