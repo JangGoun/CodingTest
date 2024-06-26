6808f86978a108876989c3403aadf85cb300209d
JangGoun <goun3596@gmail.com>
2024-06-26 17:03:51 +0900

    baekjoon day03

--- src/baekjoon/Day03/d305.java
+++ src/baekjoon/Day03/d305.java
@@ -1,4 +1,21 @@
 package baekjoon.Day03;
 
+import java.util.Scanner;
+
 public class d305 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int num = N/4;
+
+        if (num>0){
+            for (int i = 0; i < num; i++) {
+                System.out.print("long ");
+            }
+            System.out.print("int");
+        }
+
+
+        sc.close();
+    }
 }
